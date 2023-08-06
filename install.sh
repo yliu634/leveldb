fbd58a4b56636859214b58c5951d76dc17146edb
Y. Liu <34199685+yliu634@users.noreply.github.com>
2023-08-06 01:30:16 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo rm -rf /usr/lib/libspotkv.so*
+sudo rm -rf /usr/local/lib/libspotkv*
+sudo cp -R include/* /usr/local/include
+sudo cp out-shared/libspotkv.so* /usr/local/lib
+sudo cp out-shared/libspotkv.so* /usr/lib
+sudo cp out-static/libspotkv.a /usr/local/lib
